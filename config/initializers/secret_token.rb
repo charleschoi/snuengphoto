1c3a2ace00e37b46c24c7e17d5813ab3d4d24771
Charles <charleschoi87@gmail.com>
2013-05-10 16:25:38 +0900

    setting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snueng::Application.config.secret_token = '8c1ef171f23be9dc86f5d1dae17f8f089042a23b2f63b5b56bcddbf470bb10b71382ae27e42ee41bc04a870f070f856287b363ac013b7585e5740383799834cb'
