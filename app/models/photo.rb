6ed6cbc319e84722dd8f8015903296d29e62de6a
Charles <charleschoi87@gmail.com>
2013-05-31 08:55:05 +0900

    delete public/index.html

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :photo_abc, :photo_real_name, :photo_saved_name, :title, :user_id, :when, :where
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :dept_id, :email, :find_pw_code, :name, :password_hash, :phone, :student_id
+  has_many :photos
 end
